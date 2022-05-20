80e5af17d4828b9e541cf649744ba7eae64a3db7
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-05-20 19:52:13 +0200

    basic routing with basic templates

--- base/views.py
+++ base/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+# from django.http import HttpResponse
+
+def home(request):
+    # return HttpResponse('Home page')
+    return render(request, 'home.html')
+
+def room(request):
+    # return HttpResponse('ROOM')
+    return render(request, 'room.html')
+
