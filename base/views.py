c049e5fdf002034b43281760a5c3ef90692169ef
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-07-10 01:35:26 +0200

    chore: send to and access data in templates

--- base/views.py
+++ base/views.py
@@ -3,11 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 # from django.http import HttpResponse
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend Developers'},
+]
+
 def home(request):
+    context = {'rooms':rooms}
     # return HttpResponse('Home page')
-    return render(request, 'home.html')
+    return render(request, 'base/home.html', context)
 
 def room(request):
     # return HttpResponse('ROOM')
-    return render(request, 'room.html')
+    return render(request, 'base/room.html')
 
