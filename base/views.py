a3cbab727a5611610f49f29f74d39673320ae861
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-07-25 13:17:56 +0200

    chore: dynamic links using the url template tag

--- base/views.py
+++ base/views.py
@@ -1,20 +1,26 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
 # from django.http import HttpResponse
 
 rooms = [
-    {'id': 1, 'name': 'Lets learn python'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend Developers'},
+  {'id': 1, 'name': 'Lets learn python'},
+  {'id': 2, 'name': 'Design with me'},
+  {'id': 3, 'name': 'Frontend Developers'},
 ]
 
 def home(request):
-    context = {'rooms':rooms}
-    # return HttpResponse('Home page')
-    return render(request, 'base/home.html', context)
+  context = {'rooms': rooms}
+  # return HttpResponse('Home page')
+  return render(request, 'base/home.html', context)
 
-def room(request):
-    # return HttpResponse('ROOM')
-    return render(request, 'base/room.html')
+def room(request, pk):
+  room = None
+  for i in rooms:
+    if i['id'] == int(pk):
+      room = i
+  context = {'room': room}
+  # return HttpResponse('ROOM')
+  return render(request, 'base/room.html', context)
 
