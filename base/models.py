80f0497afb0ebc809a71c09623f8b42325f7309a
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-07-25 14:30:16 +0200

    chore: create model and make accessible in /admin

--- base/models.py
+++ base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+  # host =
+  # topi =
+  name = models.CharField(max_length=200)
+  description = models.TextField(null=True, blank=True)
+  # participants = 
+  updated = models.DateTimeField(auto_now=True) #whenever something changes
+  created = models.DateTimeField(auto_now_add=True) #only once, when object created
+
+  def __str__(self):
+    return self.name
+  
