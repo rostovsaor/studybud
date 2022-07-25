e3745495024f8d93a2b1c7ce70986223c51cbda1
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-07-25 14:49:57 +0200

    chore: templating with data from sqlite3 database

--- base/views.py
+++ base/views.py
@@ -1,26 +1,23 @@
-from multiprocessing import context
+# from multiprocessing import context
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 # from django.http import HttpResponse
 
-rooms = [
-  {'id': 1, 'name': 'Lets learn python'},
-  {'id': 2, 'name': 'Design with me'},
-  {'id': 3, 'name': 'Frontend Developers'},
-]
+# rooms = [
+#   {'id': 1, 'name': 'Lets learn python'},
+#   {'id': 2, 'name': 'Design with me'},
+#   {'id': 3, 'name': 'Frontend Developers'},
+# ]
 
 def home(request):
+  rooms = Room.objects.all()
   context = {'rooms': rooms}
-  # return HttpResponse('Home page')
   return render(request, 'base/home.html', context)
 
 def room(request, pk):
-  room = None
-  for i in rooms:
-    if i['id'] == int(pk):
-      room = i
+  room = Room.objects.get(id=pk)
   context = {'room': room}
-  # return HttpResponse('ROOM')
   return render(request, 'base/room.html', context)
 
