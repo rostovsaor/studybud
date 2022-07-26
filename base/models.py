b49c9eabd450d64a7b899701883d63b3b09eb8f2
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-07-26 13:58:28 +0200

    chore: create models Topic, Message, and relationships

--- base/models.py
+++ base/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
-class Room(models.Model):
-  # host =
-  # topi =
+
+class Topic(models.Model):
+  name = models.CharField(max_length=200)
+  def __str__(self):
+    return self.name
+
+class Room (models.Model):
+  host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) #Relationship, Many to one
+  # topic = models.ForeignKey('Topic', on_delete=models.CASCADE) use quotes on Topic to hoist the class
+  topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True) #Relationship, Many to one
   name = models.CharField(max_length=200)
   description = models.TextField(null=True, blank=True)
   # participants = 
@@ -14,3 +23,12 @@ class Room(models.Model):
   def __str__(self):
     return self.name
   
+class Message(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE) #Relationship, many to one
+  room = models.ForeignKey(Room, on_delete=models.CASCADE) #Relationship, many to one
+  body = models.TextField()
+  updated = models.DateTimeField(auto_now=True) #whenever something changes
+  created = models.DateTimeField(auto_now_add=True) #only once, when object created
+
+  def __str__(self):
+    return self.body[0:50] 
