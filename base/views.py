27fa177d2a99b463ec3bbf48afd544814033e0bf
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-08-01 16:20:11 +0200

    feat: show topic, room name & description search results at once

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 # from multiprocessing import context
-from multiprocessing import context
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -18,8 +18,12 @@ def home(request):
   # q = request.GET.get('q') if request.GET.get('q') else ''
   q = request.GET.get('q') or ''
 
-  # The i in icontains below defines contains as case insensitive, remove it for case sensitive. There are others such startswith etc.
-  rooms = Room.objects.filter(topic__name__icontains=q)
+  # The i in icontains below defines contains as case insensitive, remove it for case sensitive. There are others such as startswith etc.
+  rooms = Room.objects.filter(
+    Q(topic__name__icontains=q) |
+    Q(name__icontains=q) |
+    Q(description__icontains=q)
+  )
 
   topics =  Topic.objects.all()
 
