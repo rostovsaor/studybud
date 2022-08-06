633f384f0989c4a46481ebae51f0dd50ad3efb53
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-08-06 09:49:54 +0200

    feat: show rooms found count number in home

--- base/views.py
+++ base/views.py
@@ -25,9 +25,10 @@ def home(request):
     Q(description__icontains=q)
   )
 
-  topics =  Topic.objects.all()
+  topics = Topic.objects.all()
+  room_count = rooms.count() # The count() method is faster than the len() method
 
-  context = {'rooms': rooms, 'topics': topics}
+  context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
   return render(request, 'base/home.html', context)
 
 def room(request, pk):
